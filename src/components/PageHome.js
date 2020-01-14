c2399411be1d5bb1951689f37256a5d8c7c6bfe5
Rory Willis <47989883+rorywillis@users.noreply.github.com>
2020-01-13 22:12:48 -0500

    Remove About Me

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -11,7 +11,7 @@ export default function Home() {
                 <title>{site.title} | Home</title>
                 <meta name="Description" content={site.description}/>
             </Helmet>
-            <h3 className="mb-4 mt-4">About Me</h3>
+            {/* <h3 className="mb-4 mt-4">About Me</h3> */}
             <p>
                 <b>I'm Rory</b>, a full-stack developer living in Denver. I work for a leading media company and write about all things software & web development. You can find me on <a href="https://www.twitter.com/rorywillis">Twitter</a> if you want to connect!
             </p>
